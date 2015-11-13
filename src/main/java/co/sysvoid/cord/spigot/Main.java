b3e72e07da648c45f562c3619e9ac7b2fd355fad
System Void <void@sysvoid.co>
2015-11-13 00:16:24 +0000

    Did some more...

--- src/main/java/co/sysvoid/cord/spigot/Main.java
+++ src/main/java/co/sysvoid/cord/spigot/Main.java
@@ -5,14 +5,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin
 {
 
+    private static Main instance;
+
     public void onEnable()
     {
         getLogger().info("Enabled");
+        Main.instance = this;
+
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "Cord");
+        getServer().getMessenger().registerIncomingPluginChannel(this, "Cord", new MessageListener());
     }
 
     public void onDisable()
     {
         getLogger().info("Disabled");
+    }
+
+    public static Main getInstance()
+    {
+        return instance;
     }
 
 }
--- src/main/java/co/sysvoid/cord/spigot/MessageListener.java
+++ src/main/java/co/sysvoid/cord/spigot/MessageListener.java
@@ -1,5 +1,46 @@
 package co.sysvoid.cord.spigot;
 
-public class MessageListener
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+public class MessageListener implements PluginMessageListener
 {
+
+    public void onPluginMessageReceived(String channel, Player player, byte[] message)
+    {
+        if (!channel.equals("Cord"))
+        {
+            return;
+        }
+
+        ByteArrayDataInput in = ByteStreams.newDataInput(message);
+        String subchannel = in.readUTF();
+        if (subchannel.equals("Power"))
+        {
+            // Cord [Power] -> {{SERVER}}
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF(in.readUTF()); // <-- {{SERVER}}
+
+            for (Player p : Bukkit.getServer().getOnlinePlayers())
+            {
+                p.sendPluginMessage(Main.getInstance(), "Cord", out.toByteArray());
+            }
+
+            // Shutdown the server after about 5 seconds. Just to make sure *most* of the
+            // players have been moved to another server.
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable()
+            {
+                public void run()
+                {
+                    Bukkit.getServer().shutdown();
+                }
+            }, 100L);
+        }
+    }
+
 }
